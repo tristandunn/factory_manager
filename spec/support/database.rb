f3d45c517b35153eebd4bc85aa5d59d8bbaca0fd
Tristan Dunn <tristanzdunn@gmail.com>
2023-08-29 12:32:25 -0400

    Update dependencies.
    * Updates activerecord, appraisal, rubocop, rubocop-performance,
      rubocop-rspec, and sqlite3 in Ruby.
    * Adds Ruby 3.2 to CI and adjusts caching.
    * Adds the x86_64-linux platform.
    * Ignore the vendor directories for RuboCop.

--- spec/support/database.rb
+++ spec/support/database.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require "active_record"
-require_relative "./database/connection"
-require_relative "./database/schema"
-require_relative "./database/models"
-require_relative "./database/factories"
+require_relative "database/connection"
+require_relative "database/schema"
+require_relative "database/models"
+require_relative "database/factories"
